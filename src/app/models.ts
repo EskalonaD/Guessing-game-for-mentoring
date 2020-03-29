dac3d6466934749f517203ba651bd79ec55f18e5
EskalonaD <eskalonad@gmail.com>
2020-03-30 00:37:12 +0300

    Revert "add message storage models"
    This reverts commit e488ba8b682cae71f48bdfc812bf1135c2c2e1b1.

--- src/app/models.ts
+++ src/app/models.ts
@@ -2,34 +2,3 @@ export interface Message {
     text: string;
     person: 'guesser' | 'puzzler';
 }
-
-// Types of data storages for tasks output
-
-interface Task {
-    taskId: number;
-    delay: {
-      delayDuration: number;
-      baseDate: Date;
-    }
-  }
-  
-  interface TaskTimeSlot {
-    invokeIn: number; // string || date??
-    tasks: Task[];
-  }
-  
-  interface TaskByTime {
-    today: TaskTimeSlot[];
-    daily: TaskTimeSlot[];
-    weekly: TaskTimeSlot[];
-    monthly: TaskTimeSlot[];
-    yearly: TaskTimeSlot[];
-  }
-  
-  // Type for task storing
-  
-  interface TaskStorage {
-    tasksById: {
-      [key: number]: Task //not that task from tasks-output fix name collisions.
-    }
-  }
