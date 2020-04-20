7d6113d311a7acf24044c63c4161ae39cf4341cc
EskalonaD <eskalonad@gmail.com>
2020-04-20 03:23:56 +0300

    move img-source back to external resources due to gh-pages

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Message } from '@project/models';
 
 const picturesMapper = {
-    puzzler: '../assets/71560.jpg',
-    guesser: '../assets/114469.png',
+    puzzler: 'https://avatarfiles.alphacoders.com/114/71560.jpg',
+    guesser: 'https://avatarfiles.alphacoders.com/114/114469.png',
 };
 
 @Component({
